f8c517fc89b7dbe08394cf5e0248746652935f05
zegiha <leeseoyul070814@gmail.com>
2025-06-11 20:03:35 +0900

    feat: add Validation  GlobalPipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import {ValidationPipe} from '@nestjs/common'
 import { NestFactory } from '@nestjs/core';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 import { AppModule } from './app.module';
@@ -15,6 +16,8 @@ async function bootstrap() {
     jsonDocumentUrl: 'swagger/json',
   })
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap();
