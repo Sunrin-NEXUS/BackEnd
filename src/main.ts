68ae4f6a05f7c33cd6ab76811235187618538d9e
zegiha <leeseoyul070814@gmail.com>
2025-06-11 12:27:58 +0900

    chore: apply swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Nexus Core Backend')
+    .setDescription('The news viewer service, Nexus API Description')
+    .setVersion('0.1.0')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, documentConfig)
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
